0313c5a2126ca30e980f5a0ba7dcd3ebc27e03c5
Gustavo Henrique <80420403+gucervus@users.noreply.github.com>
2021-07-01 18:07:43 -0300

    modelando banco de dados

--- AjudaSP/app.py
+++ AjudaSP/app.py
@@ -1,11 +1,12 @@
 from flask import Flask, render_template, request
-from flask_sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import (SQLAlchemy)
 
 app = Flask(__name__)
 
+
 user = 'lkccpgzi'
 password = 'OSxUaq-J4Nx1L_6562tvq2v-a9_BLrVm'
-host = 'tuffi.db.elephantsql.com'
+host = 'tuffi.db.elephantsql.com '
 database = 'lkccpgzi'
 
 app.config['SQLACHEMY_DATABASE_URI'] = f'postgresql://{user}:{password}@{host}/{database}'
@@ -13,6 +14,27 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'ProjetoDB'
 
 db = SQLAlchemy(app)
+
+class Organizacoes(db.Model):
+  id = db.Column(db.Interger, primary_key = True)
+  nome_ong = db.Column(db.String(255), nullable = False)
+  url_ong = db.Column(db.String(500), nullable = False)
+
+  def __init__(self, nome_ong, url_ong):
+    self.nome_ong = nome_ong
+    self.url_ong = url_ong
+  
+  @staticmethod
+  def read_all():
+    return Organizacoes.query.order_by(Organizacoes.id.asc()).all()
+  
+  # @staticmethod
+  # def resume():
+  #   return Organizacoes.query.get(registro_id)
+
+  def save(self):
+    db.session.add(self)
+    db.session.commit()
 
 @app.route('/')
 def home():
